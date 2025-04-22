a6746959b47b89e7ccd2008d9facdd83ae93b28d
Matteo Zanotta <zetamaty@gmail.com>
2025-04-22 23:43:02 +0200

    si

--- server/db.ts
+++ server/db.ts
@@ -2,13 +2,13 @@ import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
+import 'dotenv/config';
 
 neonConfig.webSocketConstructor = ws;
 
+// Imposta la variabile d'ambiente se non è già definita
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  process.env.DATABASE_URL = "postgresql://neondb_owner:npg_buVdpqR2TAl6@ep-rapid-shadow-a4kr56m7-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require";
 }
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
