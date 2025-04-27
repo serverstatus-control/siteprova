cde599618d9f98dba4f1c0f1ac2c63ef66fb0f40
Matteo Zanotta <zetamaty@gmail.com>
2025-04-27 20:18:41 +0200

    si

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
